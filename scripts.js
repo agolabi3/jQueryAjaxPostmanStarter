ce4e915d20656a863489143e6723f81d326a03ec
agolabi3 <111457082+agolabi3@users.noreply.github.com>
2023-03-28 16:19:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,14 +10,62 @@ let cupcakesCollection = null;
 
 // use get to grab and display the cupcakes in their default order (this endpoint should return all of the cupcakes in  JSON in alphabetical order])
 //TODO
+//the end point
+$.get("https://61e93050-cc69-4fce-9d6c-418d5aaa780b.mock.pstmn.io/cupcakes", function(data){
+  //if the call fails, this annonymouse function will do this
+  //console.log the return data
+  console.log(data);
 
+  //if it successes display to the page
+  //this is the helper function that is lower down
+  displayCupcakes(data);
+
+  //add the data to the global var
+  cupcakesCollection = data;
+}, "json").fail(function(){
+  //empty out the sting
+  $("#cupcakes").empty();
+  //give them a message
+  $("#cupcakes").append("<div>There is an error while connecting.</div>");
+});//end with the data we are expecting from the surver
 
 // display sorted in ascending order (this is calling a different endpoint than the original, but you can use the same one or the global variable, we're just writing this as another call to the server so we can see the syntax for this type of call)
 //TODO
+$("#alpha").click(function(){
+  $.ajax({
+    url: "https://6f186305-ab26-4839-b806-380e3560e049.mock.pstmn.io/cupcakes.json?alpha=asc",
+    method: "GET",
+    datatype: "json"
+  })
+  .done(function(data){
+    //pars text into json
+    let json = jQuery.parseJSON(data);  
+    //display the soarted cupakes to the page
+    displayCupcakes(json);
+  })
 
+  .fail(function(){
+    //empty out the sting
+    $("#cupcakes").empty();
+    //give them a message
+    $("#cupcakes").append("<div>There is an error while connecting.</div>");
+  });
+});
 
 // display the cupcakes sorted in descending order, but we'll use some built-in methods to handle this on the cupcakes we already have rather than making another call to the endpoint 
 //TODO
+$("#zed").click(function(){
+  if(cupcakesCollection[0].name == "Bubble Gum Pop"){
+    //copu the stuff to new var
+    let backwardsCupcakes = cupcakesCollection;
+    //now revers order in new array
+    backwardsCupcakes = backwardsCupcakes.reverse();
+    //diplsay the new order of cakes
+    displayCupcakes(backwardsCupcakes);
+  }else{
+    return;
+  }
+});
 
 // helper function to display the cupcakes given the data returned from the call
 function displayCupcakes(data){
@@ -39,6 +87,7 @@ function displayCupcakes(data){
         <p class='frosting'>${cupcake.frostingIngredients}</p>
       </section>`;
   }
+  //add the html to the page
   $("#cupcakes").html(string);
 }
 
